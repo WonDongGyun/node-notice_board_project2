47597409d94de0e15ae509292ea7c20288106086
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-04-06 02:18:43 +0900

    commit

--- authentication/auth_login.js
+++ authentication/auth_login.js
@@ -1,35 +1,32 @@
-const jwt = require('jsonwebtoken');
-const { Account } = require('../models');
+const jwt = require("jsonwebtoken");
+const { Account } = require("../models");
 
 module.exports = (req, res, next) => {
-    try {
-        const { authorization } = req.headers;
-        const [tokenType, tokenValue] = authorization.split(' ');
+  try {
+    const { authorization } = req.headers;
+    const [tokenType, tokenValue] = authorization.split(" ");
 
-
-        if (tokenType != 'Bearer') {
-            return res.status(400).send({
-                result: "fail",
-                status: 400,
-                modal_title: "로그인 필요",
-                modal_body: "로그인을 해주세요."
-            });
-
-        }
-
-        const { nickname } = jwt.verify(tokenValue, 'DongGyunKey');
-
-        Account.findByPk(nickname).then((account) => {
-            res.locals.account = account;
-            next();
-        });
-    } catch (err) {
-        res.status(400).send({
-            result: "fail",
-            status: 400,
-            modal_title: "로그인 필요",
-            modal_body: "로그인을 해주세요."
-        });
-
+    if (tokenType != "Bearer") {
+      return res.status(400).send({
+        result: "fail",
+        status: 400,
+        modal_title: "로그인 필요",
+        modal_body: "로그인을 해주세요.",
+      });
     }
-}
+
+    const { nickname } = jwt.verify(tokenValue, "DongGyunKey");
+
+    Account.findByPk(nickname).then((account) => {
+      res.locals.account = account;
+      next();
+    });
+  } catch (err) {
+    res.status(400).send({
+      result: "fail",
+      status: 400,
+      modal_title: "로그인 필요",
+      modal_body: "로그인을 해주세요.",
+    });
+  }
+};
